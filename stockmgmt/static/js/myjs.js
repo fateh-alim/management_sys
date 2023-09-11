40b30913e4ac00bf70d7071e1ed0c31e911e471b
fateh alim <fateh.alim@arbisoft.com>
2023-09-11 12:21:40 +0500

    final project

---
+++ stockmgmt/static/js/myjs.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+
+  $('.table').paging({limit:5});
+
+
+  NProgress.start();
+  NProgress.done();
+
+  $(".datetimeinput").datepicker({changeYear: true,changeMonth: true, dateFormat: 'yy-mm-dd'});
+    
+
+
+
+
+
+
+
+
+
+
+    
+  });
